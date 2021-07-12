2c0cb5285ef751d2099a10d1f1bec3b7ac0f5538
Francesco <francescococlite@gmail.com>
2021-07-12 15:26:53 +0200

    push

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div v-for="(element,index) in films" :key="index"> {{element.title}} {{element.original_title}} {{element.original_language}} {{element.vote_average}}</div>
+    <form action=""><input type="text"><button type="button" class="btn btn-primary mx-3">Cerca</button></form>
+    <div v-for="(element,index) in films" :key="index"> {{element[index].title}} {{element[index].original_title}} {{element[index].original_language}} {{element[index].vote_average}}</div>
   </div>
 </template>
 
@@ -10,7 +11,7 @@ export default {
   name: 'Main',
       data(){
         return{
-            apiUrl : "https://api.themoviedb.org/3/movie/550?api_key=e074c01e562b214f49b0d0b915aa74f1",
+            apiUrl : "https://api.themoviedb.org/3/movie/popular?api_key=e074c01e562b214f49b0d0b915aa74f1",
             films : [],
             
         }
@@ -23,8 +24,9 @@ export default {
             axios
             .get(this.apiUrl)
             .then(picker =>{
-              this.films.push(picker.data);
+              this.films.push(picker.data.results);
               console.log(picker);
+              
             })
         }
     }
