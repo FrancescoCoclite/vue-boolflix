2f35405f25f0695afd82cfdcfedf5cddd96335da
Francesco <francescococlite@gmail.com>
2021-07-12 18:14:52 +0200

    search bar

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <form action=""><input type="text"><button type="button" class="btn btn-primary mx-3">Cerca</button></form>
-    <div v-for="(element,index) in films" :key="index"> {{element[index].title}} {{element[index].original_title}} {{element[index].original_language}} {{element[index].vote_average}}</div>
+    <div class="container-lg">
+      <div class="row">
+        <div class="col-12 mb-5 ">
+          <Search/>
+        </div>
+        <div class="col-12">
+
+          <div class="card d-inline-flex mx-3 mt-2" style="width: 18rem;" v-for="(element,index) in films" :key="index">
+            <div class="card-body">
+              <h5 class="card-title">{{element.title}}</h5>
+              <h6>{{element.original_title}}</h6>
+              <p class="card-text">{{element.original_language}}</p>
+              <p>{{element.vote_average}}</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+
    import axios from 'axios';
+   import Search from '../components/Search.vue'
 export default {
   name: 'Main',
       data(){
@@ -16,6 +36,9 @@ export default {
             
         }
     },
+    components :{
+      Search,
+    },
     created(){
         this.filmSelected()
     },
@@ -24,14 +47,13 @@ export default {
             axios
             .get(this.apiUrl)
             .then(picker =>{
-              this.films.push(picker.data.results);
+              this.films = picker.data.results;
               console.log(picker);
+              console.log(this.films);
               
             })
         }
     }
-
-
 }
 </script>
 
---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <form class="d-flex">
+    <div> 
+        <input type="text">
+    </div>
+    <div>
+        <button type="button" class="btn btn-primary mx-3">Cerca</button>
+    </div>
+   </form>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'Search'
+}
+</script>
+
+<style>
+
+</style>
