7404a0da80f452a288abc4623e071e9c2b46e154
Francesco <francescococlite@gmail.com>
2021-07-12 12:57:17 +0200

    problema con link api

---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-for="(element,index) in films" :key="index"> {{element.title}} {{element.original_title}} {{element.original_language}} {{element.vote_average}}</div>
+  </div>
+</template>
+
+<script>
+   import axios from 'axios';
+export default {
+  name: 'Main',
+      data(){
+        return{
+            apiUrl : "https://api.themoviedb.org/3/movie/550?api_key=e074c01e562b214f49b0d0b915aa74f1",
+            films : [],
+            
+        }
+    },
+    created(){
+        this.filmSelected()
+    },
+    methods :{
+        filmSelected(){
+            axios
+            .get(this.apiUrl)
+            .then(picker =>{
+              this.films.push(picker.data);
+              console.log(picker);
+            })
+        }
+    }
+
+
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
