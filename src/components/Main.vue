5ab43341a3fa131de3c97e8e6b165cdd6fe35db2
Francesco <francescococlite@gmail.com>
2021-07-14 11:30:18 +0200

    push

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,27 +3,29 @@
     <div class="container-lg">
       <div class="row">
         <div class="col-12"> 
-          <h2 class="text-danger" id="film"> Films</h2>
+          <h2 class="text-danger mb-5" id="film"> Films</h2>
           <div class="card d-inline-flex bg-transparent" style="width: 18rem;" v-for="(element,index) in films" :key="index">
            <img class="copertina position-relative" :src="'https://image.tmdb.org/t/p/w342' + element.poster_path" alt="copertina-film">
             <div class="card-body position-absolute">
               <h5 class="card-title text-secondary">{{element.title}}</h5>
-              <h6 d-none>{{element.original_title}}</h6>
+              <h6>{{element.original_title}}</h6>
               <p class="card-text">{{element.original_language}} <img class="flag" :src="'/img/'+element.original_language+'.png'" alt="bandiera"></p>
               <p>{{element.vote_average}}</p>
+              <p class="overview">{{element.overview}}</p>
             </div>
           </div>
 
         </div>
         <div class="col-12">
-          <h2 class="text-danger" id="serie">Serie tv</h2>
+          <h2 class="text-danger mt-5" id="serie">Serie tv</h2>
           <div class="card d-inline-flex bg-transparent" style="width: 18rem;" v-for="(element,index) in series" :key="index">
-            <img class="copertina position-relative" :src="'https://image.tmdb.org/t/p/w342' + element.poster_path" alt="copertina-serie">
+            <img class="copertina position-relative" :src="'https://image.tmdb.org/t/p/w342' + element.poster_path" onerror="this.src='/img/img404.jpg'" alt="copertina-serie">
             <div class="card-body position-absolute">
               <h5 class="card-title text-secondary">{{element.name}}</h5>
               <h6>{{element.original_title}}</h6>
               <p class="card-text">{{ element.original_language }}<img class="flag" :src="'/img/'+element.original_language+'.png'" alt="bandiera"></p>
               <p>{{element.vote_average}}</p>
+              <p class="overview">{{element.overview}}</p>
             </div>
           </div>
 
@@ -56,11 +58,11 @@ export default {
     height: 30px;
     width: 30px;
   }
+  .card:hover .copertina{
+    z-index: -1;
+  }
   .copertina{
     z-index: 999;
-    &:active{
-      z-index: 00;
-    }
   }
   .card-body{
     background-color: black;
@@ -69,6 +71,10 @@ export default {
   }
   .sfondo{
     height: 100%;
+  }
+  .overview{
+    font-size: 10px;
+    grid-template-columns: 1fr, 40ch 1fr;
   }
   
   
